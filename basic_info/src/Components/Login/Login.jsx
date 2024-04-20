a346a8039c70dd119e28c55483f3503cda386a8e
alujan <alujan@unal.edu.co>
2024-04-20 11:59:29 -0500

    Styling and include of MUI components

--- basic_info/src/Components/Login/Login.jsx
+++ basic_info/src/Components/Login/Login.jsx
@@ -3,6 +3,16 @@ import React from 'react'
 // Importing of CSS for Login
 import './Login.css'
 
+// Import of Material UI components
+import { styled } from '@mui/material/styles';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField';
+
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import HttpsOutlinedIcon from '@mui/icons-material/HttpsOutlined';
+
 // Import of assets: Icons for Login
 import user_icon from '../Assets/usuario.png'
 import password_icon from '../Assets/candado.png'
@@ -14,38 +24,53 @@ import email_icon from '../Assets/email.png'
 // <a href="https://www.flaticon.es/iconos-gratis/correo-electronico" title="correo electrónico iconos">Correo electrónico iconos creados por Freepik - Flaticon</a>
 // <a href="https://www.flaticon.es/iconos-gratis/informacion" title="informacion iconos">Información iconos creados por deemakdaksina - Flaticon</a>
 
+const SignInButton = styled(Button)(({ theme }) => ({
+    color: '#005f6f',
+    borderColor: '#005f6f',
+    '&:hover': { borderColor: '#039CBA', color: '#039CBA' },
+  }));
+
+const SignUpButton = styled(Button)(({ theme }) => ({
+    color: '#FFF',
+    backgroundColor: '#005f6f',
+    '&:hover': { backgroundColor: '#039CBA', color: '#FFF' },
+  }));
+
 const Login = () => {
-  return (
+    return (
 
-    <div className='container'>
-        
-        <div className='header'>
-            <div className='text'>Log in</div>
-            <div className='underline'></div>
-        </div>
+        <div className='container'>
 
-        <div className='inputs'>
-            <div className='input'>
-                <img src={user_icon} alt='user: ' />
-                <input type='user' />
+            <div className='header'>
+                <div className='text'>Log in</div>
+                <div className='underline'></div>
             </div>
-            <div className='input'>
-                <img src={password_icon} alt='password: '/>
-                <input type='password' />
-            </div>
-            <div className='input'>
-                <img src={email_icon} alt='email'/>
-                <input type='email' />
-            </div>
-        </div>
 
-        <div className='forgot-password'><span>Did you forgot your password?</span></div>
-        <div className='submit-containter'>
-            <div className='submit'>Sign Up</div>
-            <div className='submit'>Login</div>
+            <div className='inputs'>
+                <Box sx={{ display: 'flex', alignItems: 'flex-end', my: 2 }}>
+                    <PermIdentityIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                    <TextField id="Username" label="Username" variant="standard" />
+                </Box>
+
+                <Box sx={{ display: 'flex', alignItems: 'flex-end', my: 2 }}>
+                    <HttpsOutlinedIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                    <TextField id="password" label="Password" variant="standard" />
+                </Box>
+            </div>
+
+            <div className='submit-container'>
+                <SignInButton variant="outlined">
+                    Sign in
+                </SignInButton>
+
+                <SignUpButton variant="filled">
+                    Sign up
+                </SignUpButton>
+            </div>
+
+            <div className='forgot-password'><span>Did you forgot your password?</span></div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login
